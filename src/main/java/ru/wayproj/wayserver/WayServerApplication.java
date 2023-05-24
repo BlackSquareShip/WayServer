42eedd6cf828fbbca9f8a6fc030f2c76b98dafdf
BlackSquareShip <danilg2002@mail.ru>
2023-05-24 12:44:10 +0200

    fix

---
+++ src/main/java/ru/wayproj/wayserver/WayServerApplication.java
@@ -0,0 +1,14 @@
+package ru.wayproj.wayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class WayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WayServerApplication.class, args);
+    }
+
+}
